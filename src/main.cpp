0f3a18b44e8cdcccdc2cc9a9e92d8f84287ab1f0
OakFoil <elsennahway@gmail.com>
2026-07-16 08:52:06 +0300

    simplify code

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,36 @@ const int LED_PIN = 2,
           RECIEVER_PIN = 23,
           bufferSize = 1024,
           timeout = 50,
-          rawMessageTransmitFreq = 38000;
+          rawrecieved_messageTransmitFreq = 38000;
 
 IRrecv reciever(RECIEVER_PIN, bufferSize, timeout);
 IRsend transmitter(LED_PIN);
 decode_results recieved_message;
 
-void printMessageInfo(const decode_results &message)
+void printMessageInfo()
 {
-    Serial.print(resultToHumanReadableBasic(&message));
-    String description = IRAcUtils::resultAcToString(&message);
+    Serial.print(resultToHumanReadableBasic(&recieved_message));
+    String description = IRAcUtils::resultAcToString(&recieved_message);
     if (description.length())
         Serial.println(D_STR_MESGDESC ": " + description);
-    Serial.println(resultToTimingInfo(&message));
-    Serial.println(resultToSourceCode(&message));
+    Serial.println(resultToTimingInfo(&recieved_message));
+    Serial.println(resultToSourceCode(&recieved_message));
 }
 
-bool transmitMessage(IRsend &transmitter, const decode_results &message)
+bool transmitMessage()
 {
     bool success = true;
-    if (message.decode_type == decode_type_t::UNKNOWN)
+    if (recieved_message.decode_type == decode_type_t::UNKNOWN)
     {
-        uint16_t *raw_array = resultToRawArray(&message);
-        uint16_t correctedLen = getCorrectedRawLength(&message);
-        transmitter.sendRaw(raw_array, correctedLen, rawMessageTransmitFreq);
+        uint16_t *raw_array = resultToRawArray(&recieved_message);
+        uint16_t correctedLen = getCorrectedRawLength(&recieved_message);
+        transmitter.sendRaw(raw_array, correctedLen, rawrecieved_messageTransmitFreq);
         delete[] raw_array;
     }
-    else if (hasACState(message.decode_type))
-        success = transmitter.send(message.decode_type, message.state, message.bits / 8);
+    else if (hasACState(recieved_message.decode_type))
+        success = transmitter.send(recieved_message.decode_type, recieved_message.state, recieved_message.bits / 8);
     else
-        success = transmitter.send(message.decode_type, message.value, message.bits);
+        success = transmitter.send(recieved_message.decode_type, recieved_message.value, recieved_message.bits);
 
     return success;
 }
@@ -51,15 +51,15 @@ void setup()
     transmitter.begin();
 
     while (!reciever.decode(&recieved_message))
-        ; // Wait until message is recieved
+        ; // Wait until recieved_message is recieved
 
-    printMessageInfo(recieved_message);
+    printMessageInfo();
 }
 
 void loop()
 {
-    bool success = transmitMessage(transmitter, recieved_message);
-    Serial.printf("A %d-bit %s message was %ssuccessfully retransmitted.\n",
+    bool success = transmitMessage();
+    Serial.printf("A %d-bit %s recieved_message was %ssuccessfully retransmitted.\n",
                   recieved_message.bits, typeToString(recieved_message.decode_type).c_str(), success ? "" : "un");
 
     delay(1000);
